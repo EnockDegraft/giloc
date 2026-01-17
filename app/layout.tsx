98086a8533161ce1df1adeb28c7a1203fe3e402a
unknown <Sody@8t54r.(none)>
2026-01-17 08:38:16 +0000

    v2 from home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import "@/app/globals.css"
-import { Suspense } from "react"
-import { Inter } from "next/font/google"
-import { Analytics } from "@vercel/analytics/react"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Loading } from "@/components/loading"
+import "@/app/globals.css";
+import { Suspense } from "react";
+import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Loading } from "@/components/loading";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = localFont({
+  src: "./fonts/Inter-VariableFont_opsz,wght.ttf",
+  variable: "--font-inter",
+});
 
 export const metadata = {
   title: "Global Integrity Leaders Of Change",
-  description: "Empowering young entrepreneurs, developing trustworthy leaders, and nurturing talents across Africa.",
-    generator: 'v0.app'
-}
+  description:
+    "Empowering young entrepreneurs, developing trustworthy leaders, and nurturing talents across Africa.",
+};
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <head>
-        <title>GILOC - Global Integrity Leaders Of Change</title>
-      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="white" enableSystem>
           <Suspense
@@ -35,5 +39,5 @@ export default function RootLayout({ children }) {
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
